85858f908c779562836390d0b33aa994872a1138
Rafael Uzarowski <rafael.uzarowski@sevenval.com>
2015-09-12 17:23:04 +0200

    Remove boeffla-config-reset-v4.zip from ramdisk to reduce recovery size

--- cm.mk
+++ cm.mk
@@ -6,12 +6,12 @@ PRODUCT_COPY_FILES += \
     $(RAMDISK_PATH)/fs/sbin/boeffla-init.sh:root/sbin/boeffla-init.sh \
     $(RAMDISK_PATH)/fs/sbin/tinyplay:root/sbin/tinyplay \
     $(RAMDISK_PATH)/fs/sbin/busybox:root/sbin/busybox \
-    $(RAMDISK_PATH)/fs/res/misc/boeffla-config-reset-v4.zip:root/res/misc/boeffla-config-reset-v4.zip \
     $(RAMDISK_PATH)/fs/res/misc/su:root/res/misc/su \
     $(RAMDISK_PATH)/fs/res/misc/silence.wav:root/res/misc/silence.wav \
     $(RAMDISK_PATH)/fs/res/misc/install-recovery.sh:root/res/misc/install-recovery.sh \
     $(RAMDISK_PATH)/fs/res/bc/bccontroller.sh:root/res/bc/bccontroller.sh \
     $(LOCAL_PATH)/init.rc:root/init.rc
+#    $(RAMDISK_PATH)/fs/res/misc/boeffla-config-reset-v4.zip:root/res/misc/boeffla-config-reset-v4.zip
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.adb.secure=0 \
